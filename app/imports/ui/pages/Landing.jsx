ccc3446b07bbe2a69cb52ce7626a0633559f5ab0
dkihe <dnkkihe@gmail.com>
2018-10-24 11:26:16 -1000

    Pie Chart

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Image } from 'semantic-ui-react';
 import Graph1 from '../components/Graph1'
+import Graph2 from '../components/Graph2'
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -10,12 +11,12 @@ class Landing extends React.Component {
 
           <Grid.Row>
             <Grid.Column width={8}>
-            <Graph1/>
-          </Grid.Column>
+              <Graph1/>
+            </Grid.Column>
 
-          <Grid.Column width={8}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+            <Grid.Column width={8}>
+              <Graph2/>
+            </Grid.Column>
           </Grid.Row>
 
           <Grid.Row>
