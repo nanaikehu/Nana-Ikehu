77d238cd85983e546adbc0a0408fc6583d4cfd24
Nguyen Banh <banhtuannguyen@gmail.com>
2018-11-07 18:13:33 -1000

    edit map
    Add coordination of building.
    Add markers to map

--- app/imports/ui/components/Map.jsx
+++ app/imports/ui/components/Map.jsx
@@ -2,82 +2,51 @@
 
 import React, { Component, createRef } from 'react'
 import { Map, TileLayer, Marker, Popup, Rectangle,} from 'react-leaflet'
-import { Card, Container} from 'semantic-ui-react';
-import HeatmapLayer from 'react-leaflet-heatmap-layer';
+import { Card, Container} from 'semantic-ui-react'
+import defaultBuilding from '../../api/building_db/buildingCoor'
+import { _ } from 'meteor/underscore';;
+import PropTypes from 'prop-types';
+import Building from '../pages/Building';
+import { Link } from 'react-router-dom';
 
-const addressPoints =[[21.30389991490436, -157.81513986177745, "200"], [21.30141455749965, -157.82052573747936, "500"], [21.301293240432628,  -157.81430925708267, "1000"], [21.29705324949744, -157.8167632687837, "2000"], [21.300067998820815, -157.8209458384663, "5000"], [21.297467503959826, -157.82024481799456, "10000"]];
-const biomedical = [[21.30389991490436, -157.81513986177745], [21.30305957111875, -157.8141431696713]];
-const business = [[21.30141455749965, -157.82052573747936], [21.300744911029966, -157.81990413554016]];
-const korean = [[21.301293240432628,  -157.81430925708267], [21.30097231784676, -157.81407800037414]];
-const eng = [[21.29705324949744, -157.8167632687837], [21.296801002358706, -157.81545300967994]];
-const architecture = [[21.300067998820815, -157.8209458384663], [21.29946402306616, -157.82051257789135]];
-const bachman = [[21.297467503959826, -157.82024481799456], [21.297123535000768, -157.8197948355228]];
+
 
 
 export default class Map1 extends Component {
-  state = {
-    center: {
-      lat: 21.299677843574493,
-      lng: -157.81743038445714
-    },
-    marker: {
-      lat: 21.299677843574493,
-      lng: -157.81743038445714
-    },
-    zoom: 16,
-    draggable: true,
+  constructor(props) {
+    super(props)
+    this.state = { marker: this.props.marker };
   }
 
-  refmarker = React.createRef()
 
 
-  style(feature) {
-    return {
-      fillColor: getColor(feature.properties.density),
-      weight: 2,
-      opacity: 1,
-      color: 'white',
-      dashArray: '3',
-      fillOpacity: 0.7
-    };
-  }
 
-  toggleDraggable = () => {
-    this.setState({ draggable: !this.state.draggable })
-  }
-
-  updatePosition = () => {
-    const marker = this.refmarker.current
-    console.log(this.refmarker.current)
-    if (marker != null) {
-      this.setState({
-        marker: marker.leafletElement.getLatLng(),
-      })
-    }
-  }
 
   render() {
-    const position = [this.state.center.lat, this.state.center.lng]
-    const markerPosition = [this.state.marker.lat, this.state.marker.lng]
+    const position = [21.299677843574493, -157.81743038445714]
 
     return (
         <Card>
-          <Map center={position} zoom={this.state.zoom} minZoom={'16'} style={{height: '600px'}}>
+          <Map center={position} zoom={16} minZoom={'16'} style={{height: '600px'}}>
             <TileLayer
                 attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
             />
-            <Marker
-                draggable={this.state.draggable}
-                onDragend={this.updatePosition}
-                position={markerPosition}
-                ref={this.refmarker}>
-              <Popup minWidth={90}>
-            <span onClick={this.toggleDraggable}>
-              {this.state.draggable ? 'DRAG MARKER' : 'MARKER FIXED'}
+            {
+              defaultBuilding.map( item =>{
+            let x = {lat: item.coor[0], lon: item.coor[1]}
+             return <Marker position={x}>
+                <Popup minWidth={90}>
+            <span>
+              <Link to="/building">Building</Link>
             </span>
-              </Popup>
-            </Marker>
+                </Popup>
+              </Marker>}
+
+            )}
+
+
+
           </Map>
         </Card>
     )
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,19 +5,20 @@ import { Meteor } from "meteor/meteor";
 import { Graph_SimpleLine } from '../components/Graph_SimpleLine';
 import { Graph1 } from '../components/Graph1';
 import { Building } from '../pages/Building';
+import { _ } from 'meteor/underscore';
+
 import Map1 from '../components/Map'
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
+
   render() {
     return (
         <div className = 'landing-background'>
 
         <Container fluid>
           <Header inverted textAlign={'center'} as='h2'>Energy Dashboard</Header>
-          {/*<Graph_SimpleLine meterId={5} x={'time'} y={'mean'}/>*/}
-          <Card.Group itemsPerRow={2}>
-            <Graph_SimpleLine meterId={5} x={'time'} y={'mean'}/>
+          <Card.Group itemsPerRow={1}>
             <Map1/>
           </Card.Group>
         </Container>
