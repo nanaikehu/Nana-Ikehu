ddaa88023839bd0022c3eaeb355b7bdeb7a5407a
dkihe <dnkkihe@gmail.com>
2018-11-15 07:30:44 -1000

    Style for Long Scroll

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Container } from 'semantic-ui-react';
+import { Image, Container, Icon } from 'semantic-ui-react';
 
 
 
@@ -25,15 +25,30 @@ class Landing extends React.Component {
           </div>
           <div className='landing-about'>
             <div className='text'>
-              <h1>Welcome to Nānā Ikehu</h1>
+              <h1>Welcome to</h1>
+              <h1>Nānā Ikehu</h1>
               <p>Nānā Ikehu visualizes energy usage throughout the University of Hawaii campus through the use of graphs and maps. Users are able to see the amount of energy used for each building by either clicking a building on the campus map, or by selecting a building through the drop down menu.</p>
             </div>
+            <Image size='big' floated='right' src='/images/scr1.png' />
           </div>
-          <div className='landing-data'>
+          <div className='landing-graphs'>
             <div className='text'>
-              <h1>Welcome to Nānā Ikehu</h1>
+              <h1>Visualize Energy</h1>
               <p>This app visualizes energy usage throughout the University of Hawaii campus through the use of graphs and maps. Users are able to see the amount of energy used for each building by either clicking a building on the campus map, or by selecting a building through the drop down menu.</p>
             </div>
+            <div className='graph-icons'>
+              <Icon inverted name='line graph' size='massive' />
+              <Icon inverted name='pie graph' size='massive' />
+              <Icon inverted name='area graph' size='massive' />
+            </div>div
+          </div>
+          <div className='landing-about'>
+            <div className='text'>
+              <h1>Welcome to</h1>
+              <h1>Nānā Ikehu</h1>
+              <p>Nānā Ikehu visualizes energy usage throughout the University of Hawaii campus through the use of graphs and maps. Users are able to see the amount of energy used for each building by either clicking a building on the campus map, or by selecting a building through the drop down menu.</p>
+            </div>
+            <Image size='big' floated='right' src='/images/scr2.png' />
           </div>
         </Container>
 
