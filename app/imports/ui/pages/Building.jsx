9bbdfb5706d5eb693b418412cf910cf95dfedce1
nixonkingko <nkko@hawaii.edu>
2018-11-19 04:40:58 -1000

    adding images

--- app/imports/ui/pages/Building.jsx
+++ app/imports/ui/pages/Building.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
-import { Dropdown, Loader, Card, Grid, Container } from 'semantic-ui-react';
+import { Dropdown, Loader, Card, Grid, Container, Image } from 'semantic-ui-react';
 import { _ } from 'meteor/underscore';
-import { Meteor } from "meteor/meteor";
+import { Meteor } from 'meteor/meteor';
 import DatePicker from 'react-date-picker';
 import { Graph_LineBrush } from '../components/Graph_LineBrush';
-import MeterTextSum from '../components/MeterTextSum'
+import MeterTextSum from '../components/MeterTextSum';
 
 export default class Building extends React.Component {
 
   constructor(props) {
     super(props)
-    let today = new Date()
-    let priorDate = new Date().setDate(today.getDate()-30)
-    this.state = { data: '',  dateStart: new Date(priorDate),   dateEnd: today, meter:''};
+    const today = new Date();
+    const priorDate = new Date().setDate(today.getDate() - 30);
+    this.state = { data: '', dateStart: new Date(priorDate), dateEnd: today, meter: '' };
     this.DropdownList = this.DropdownList.bind(this);
-    this.onBuilding = this.onBuilding.bind(this)
-    this.DropdownMeterList = this.DropdownMeterList.bind(this)
-    this.meterSelected = this.meterSelected.bind(this)
-    this.endChange = this.endChange.bind(this)
-    this.startChange = this.startChange.bind(this)
+    this.onBuilding = this.onBuilding.bind(this);
+    this.DropdownMeterList = this.DropdownMeterList.bind(this);
+    this.meterSelected = this.meterSelected.bind(this);
+    this.endChange = this.endChange.bind(this);
+    this.startChange = this.startChange.bind(this);
 
-    if(this.props.match.params.code){
+    if (this.props.match.params.code){
       this.state.build = this.props.match.params.code;
     }
 
   }
 
-  endChange = date => {this.setState({ dateEnd: date }); console.log(this.state)}
-  startChange = date => this.setState({ dateStart: date })
+  endChange = date => {this.setState({ dateEnd: date }); console.log(this.state)};
+  startChange = date => this.setState({ dateStart: date });
 
   componentWillMount() {
     const self = this;
     Meteor.call("getBuildings", (error, response) => {
       if (error) {
-        console.log('Building' + error)
+        console.log('Building' + error);
       } else {
-        console.log("res+ build ")
-        console.log(response)
+        console.log("res+ build ");
+        console.log(response);
 
           self.setState({ data: response});
 
@@ -46,15 +46,15 @@ export default class Building extends React.Component {
   }
 
   DropdownList() {
-    let builds = this.state.data;
-    let selection = [];
+    const builds = this.state.data;
+    const selection = [];
     _.forEach(builds, build => {
-      let x = {
+      const x = {
         key: build.code,
         value: build.code,
-        text: build.name
+        text: build.name,
       }
-      selection.push(x)
+      selection.push(x);
     })
 
     return selection;
@@ -62,35 +62,32 @@ export default class Building extends React.Component {
 
   DropdownMeterList() {
     if (this.state.build) {
-      console.log(this.state.data)
-      let selected = _.findWhere(this.state.data, { code: this.state.build });
+      console.log(this.state.data);
+      const selected = _.findWhere(this.state.data, { code: this.state.build });
 
-      let selection = [];
+      const selection = [];
       _.forEach(selected.meters, build => {
-        let x = {
+        const x = {
           key: build.id,
-          value: build.id + " " + build.unit + " " + build.name,
-          text: build.unit + " " + build.name,
+          value: build.id + '' + build.unit + '' + build.name,
+          text: build.unit + '' + build.name,
         }
-        selection.push(x)
+        selection.push(x);
       })
-      if(this.state.meter === ''){
-      this.setState( {meter : selection[0].key, unit: 'kW'})
+      if (this.state.meter === '') {
+      this.setState( {meter : selection[0].key, unit: 'kW'});
       }
       return (
           <Dropdown placeholder='Select Meter' fluid search selection options={selection} onChange={this.meterSelected} defaultValue={selection[0].value}/>
-
       );
     }
   }
 
 
   meterSelected(e, name) {
-    let x = name.value.split(" ")
+    const x = name.value.split( '' );
     this.setState({ meter: parseInt(x[0]), unit: x[1]});
  }
-
-
 
   render() {
 
@@ -99,15 +96,17 @@ export default class Building extends React.Component {
 
   onBuilding(e, name) {
     this.setState({ build: name.value });
-    console.log("build ID: " + name.value)
+    console.log('build ID: ' + name.value);
   }
 
   renderGraph() {
-    let pad = {marginTop : '4em'}
-    let barpad = {marginBottom : '8px'}
-    const pickerColor = { color: '#fff' }
-    const style = { textAlign: 'center' }
+    const pad = {marginTop : '4em'};
+    const barpad = {marginBottom : '8px'};
+    const pickerColor = { color: '#fff' };
+    const style = { textAlign: 'center' };
     return (
+        <Container fluid>
+          <div className='building-bg'>
         <div style={pad}>
           <Grid columns={2} centered>
 
@@ -143,8 +142,9 @@ export default class Building extends React.Component {
             { (this.state.meter) && <Graph_LineBrush meterId={this.state.meter} x={'time'} y={'mean'} dateStart={this.state.dateStart.toString()} dateEnd={this.state.dateEnd.toString()}/> }
           </Card.Group>
           </Container>
-
         </div>
+        </div>
+        </Container>
     );
   }
 
