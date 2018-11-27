d5e676edad05cdd05119137706e03ebe00c4563e
Nguyen Banh <banhtuannguyen@gmail.com>
2018-11-27 11:47:15 -1000

    create HeatMap, trim down Map, button change to HeatMap

--- app/imports/ui/components/Map.jsx
+++ app/imports/ui/components/Map.jsx
@@ -2,12 +2,10 @@
 
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup} from 'react-leaflet'
-import { Container, Grid, Button, Confirm, Header } from 'semantic-ui-react'
+import { Container} from 'semantic-ui-react'
 import defaultBuilding from '../../api/building_db/buildingCoor'
 import { Link } from 'react-router-dom';
 import BuildingForMap from './BuildingForMap'
-import Calendar from 'react-calendar';
-import { _ } from 'meteor/underscore';
 
 
 
@@ -18,61 +16,12 @@ export default class Map1 extends Component {
     this.state = { data: '', marker: this.props.marker,  dateStart: '',   dateEnd: '', meter:''};
     this.state.dateStart = this.props.dateStart;
     this.state.dateEnd = this.props.dateEnd;
-
   }
 
-
-
-  componentDidUpdate(prevProps) {
-    // Typical usage (don't forget to compare props):
-
-    if (this.props.dateStart !== prevProps.dateStart || this.props.dateEnd !== prevProps.dateEnd ) {
-      let self = this;
-      this.setState({data : ''})
-      Meteor.call("sumByDate", new Date(this.props.dateStart) , new Date(this.props.dateEnd),(error, response) => {
-        if(error) {
-          console.log('SimpleLine' + error)
-        } else {
-          console.log("res + for ID " + this.state.meterId)
-
-          console.log('data from map' + response)
-
-          self.setState({ data : response });
-        }
-      });
-    }
-  }
-
-  componentWillMount() {
-    const self = this;
-    Meteor.call("sumByDate", new Date(this.props.dateStart) , new Date(this.props.dateEnd),(error, response) => {
-      if(error) {
-        console.log('SimpleLine' + error)
-      } else {
-
-        self.setState({ data : response })
-      }
-    });
-
-  }
-
-  combineDada(){
-    const data = [_.pluck(defaultBuilding, 'code'), _.pluck(defaultBuilding, 'coor')]
-    defaultBuilding.map( item =>{
-      let x = {lat: item.coor[0], lon: item.coor[1]}
-      return
-
-    }
-
-    )
-  }
 
   render()
 {
     const position = [21.299677843574493, -157.81743038445714]
-    const data = [_.pluck(defaultBuilding, 'code'), _.pluck(defaultBuilding, 'coor')]
-    console.log(data)
-    console.log(this.state.data)
     return (
 
           <Map center={position} zoom={17} minZoom={'17'} style={{height: '600px'}}>
--- app/imports/ui/pages/ListMap.jsx
+++ app/imports/ui/pages/ListMap.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Button, Card, Confirm, Container, Grid, Header } from 'semantic-ui-react';
+import { Button, Card, Confirm, Container, Grid, Header, Loader } from 'semantic-ui-react';
 import Map1 from '../components/Map'
+import Map2 from '../components/HeatMap'
 import Calendar from "react-calendar";
 
 
@@ -10,20 +11,43 @@ class ListMap extends React.Component {
     super(props)
     let today = new Date('2018/11/01')
     let priorDate = new Date().setDate(today.getDate()-30)
-    this.state = { open: false, dateStart: new Date(priorDate), dateEnd: new Date(today)};
+    this.state = {map: {}, open: false, dateStart: new Date(priorDate), dateEnd: new Date(today)};
     this.endChange = this.endChange.bind(this)
     this.startChange = this.startChange.bind(this)
 
   }
-  open = () => this.setState({ open: true })
-  close = () => this.setState({ open: false })
   endChange = date => {this.setState({ dateEnd: date }); console.log(this.state)}
   startChange = date => this.setState({ dateStart: date })
+  open = () => this.setState({ open: true })
+  close = () => this.setState({ open: false })
+  open1 = () => this.setState({ open: false })
+  close1 = () => this.setState({ open: true })
+
+
+  componentDidUpdate(prevProps) {
+    if (this.state.open !== prevProps.open) {
+      if(this.state.open){
+        this.state.map = <Map2 dateStart={this.state.dateStart.toString()} dateEnd={this.state.dateEnd.toString()}/>;
+      }
+      else{
+        this.state.map = <Map1 dateStart={this.state.dateStart.toString()} dateEnd={this.state.dateEnd.toString()}/>;
+      }
+    }
+  }
+  componentWillMount() {
+    if(this.state.open){
+      this.state.map = <Map2 dateStart={this.state.dateStart.toString()} dateEnd={this.state.dateEnd.toString()}/>;
+    }
+    else{
+      this.state.map = <Map1 dateStart={this.state.dateStart.toString()} dateEnd={this.state.dateEnd.toString()}/>;
+    }
+  }
 
   render() {
     const pickerColor = { color: '#fff' }
     const style = { textAlign: 'center' }
     const pad = {marginTop : '4em'}
+
     return (
         <Grid columns={2} centered>
           <Grid.Row style={pad} columns={3}>
@@ -35,6 +59,10 @@ class ListMap extends React.Component {
               <p/>
               <Button onClick={this.open}>Show</Button>
               <Confirm open={this.state.open} onCancel={this.close} onConfirm={this.close} />
+              {/*<Header inverted>Change to Marker Map</Header>*/}
+              {/*<p/>*/}
+              {/*<Button onClick={this.open1}>Show</Button>*/}
+              {/*<Confirm open={this.state.open} onCancel={this.close1} onConfirm={this.close1} />*/}
             </Grid.Column>
             <Grid.Column style={style}><Calendar className='datePicker' style={pickerColor}
                                                  value={this.state.dateEnd}
@@ -42,7 +70,7 @@ class ListMap extends React.Component {
           </Grid.Row>
           <Grid.Row>
             <Container>
-              <Map1 dateStart={this.state.dateStart.toString()} dateEnd={this.state.dateEnd.toString()}/>
+              {this.state.map}
             </Container>
           </Grid.Row>
         </Grid>
