d3c0c2eb1e031b10a27bfda52fcf6f7112d4c43b
Nguyen Banh <banhtuannguyen@gmail.com>
2018-12-01 10:57:28 -1000

    Revert "HeatMap shows the heat"
    This reverts commit 192f53f9bca3732ea8aee11320c830d36335ef0e.

--- app/imports/ui/components/HeatMap.jsx
+++ app/imports/ui/components/HeatMap.jsx
@@ -1,38 +1,41 @@
 // @flow
 
 import React, { Component } from 'react';
-import { Map, Marker, Popup,TileLayer, LayersControl, FeatureGroup } from 'react-leaflet';
+import { Map, TileLayer } from 'react-leaflet';
 import PropTypes from 'prop-types';
-import HeatmapLayer from 'react-leaflet-heatmap-layer';
+/*
+import { Container, Grid, Button, Confirm, Header } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import { _ } from 'meteor/underscore';
 import defaultBuilding from '../../api/building_db/buildingCoor';
-import addressPoints from '../../api/building_db/HeatMapData';
-import { Meteor } from "meteor/meteor";
-
+import BuildingForMap from './BuildingForMap';
+*/
 
 
 export default class Map2 extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: '', HeatData: '', marker: this.props.marker, dateStart: '', dateEnd: '', meter: '' };
+    this.state = { data: '', marker: this.props.marker, dateStart: '', dateEnd: '', meter: '' };
     this.state.dateStart = this.props.dateStart;
     this.state.dateEnd = this.props.dateEnd;
   }
 
+
   // componentDidUpdate(prevProps) {
   //   // Typical usage (don't forget to compare props):
   //
-  //   if (this.props.dateStart !== prevProps.dateStart || this.props.dateEnd !== prevProps.dateEnd) {
-  //     const self = this;
-  //     this.setState({ data: '' });
-  //     Meteor.call('sumByDate', new Date(this.props.dateStart), new Date(this.props.dateEnd), (error, response) => {
-  //       if (error) {
-  //         // console.log(`SimpleLine${error}`);
+  //   if (this.props.dateStart !== prevProps.dateStart || this.props.dateEnd !== prevProps.dateEnd ) {
+  //     let self = this;
+  //     this.setState({data : ''})
+  //     Meteor.call("sumByDate", new Date(this.props.dateStart) , new Date(this.props.dateEnd),(error, response) => {
+  //       if(error) {
+  //         console.log('SimpleLine' + error)
   //       } else {
-  //         // console.log(`res + for ID ${this.state.meterId}`);
-  //         // console.log(response);
+  //         console.log("res + for ID " + this.state.meterId)
   //
-  //         self.setState({ data: response });
+  //         console.log('data from map' + response)
+  //
+  //         self.setState({ data : response });
   //       }
   //     });
   //   }
@@ -40,49 +43,41 @@ export default class Map2 extends Component {
   //
   // componentWillMount() {
   //   const self = this;
-  //   Meteor.call('sumByDate', new Date(this.props.dateStart), new Date(this.props.dateEnd), (error, response) => {
-  //     if (error) {
-  //       // console.log(`SimpleLine${error}`);
+  //   Meteor.call("sumByDate", new Date(this.props.dateStart) , new Date(this.props.dateEnd),(error, response) => {
+  //     if(error) {
+  //       console.log('SimpleLine' + error)
   //     } else {
-  //        // console.log(response);
-  //        // console.log(this.props.dateStart);
-  //        // console.log(this.props.dateEnd);
-  //       self.setState({ data: response });
-  //     defaultBuilding.map( item =>{(this.state.data.buildings.map(e => {(e.name === item.name) ? (item.value = e.sum) : null}))});
-  //       self.setState({ HeatData: defaultBuilding });
   //
+  //       self.setState({ data : response })
   //     }
   //   });
+  //
   // }
 
-
+  // combineDada(){
+  //   const data = [_.pluck(defaultBuilding, 'code'), _.pluck(defaultBuilding, 'coor')]
+  //   defaultBuilding.map( item =>{
+  //     let x = {lat: item.coor[0], lon: item.coor[1]}
+  //     item.code;
+  //     return
+  //
+  //   }
+  //
+  //   )
+  // }
 
   render() {
     const position = [21.299677843574493, -157.81743038445714];
-    // const dataCoor = _.pluck(this.state.HeatData, "coor")
-    // const dataValue = _.pluck(this.state.HeatData, "value");
-    //
-    // console.log(dataCoor)
-    // console.log(dataValue)
+
     return (
 
         <Map center={position} zoom={17} minZoom={'17'} style={{ height: '600px' }}>
-          <HeatmapLayer
-              fitBoundsOnLoad
-              fitBoundsOnUpdate
-              points={addressPoints}
-              longitudeExtractor={m => m[1]}
-              latitudeExtractor={m => m[0]}
-              intensityExtractor={m => parseFloat(m[2])} />
             <TileLayer
                 attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
             />
 
           </Map>
-
-          
-
     );
   }
 }
