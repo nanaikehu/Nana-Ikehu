6ab6509eae46ebe052ce4e8cc7173c7ccc6c4f39
dkihe <dnkkihe@gmail.com>
2018-12-01 23:09:02 -1000

    Added Error Message

--- app/imports/ui/components/MeterTextSum.jsx
+++ app/imports/ui/components/MeterTextSum.jsx
@@ -7,7 +7,10 @@ import { Meteor } from 'meteor/meteor';
 export default class MeterTextSum extends React.Component {
   constructor(props) {
     super(props);
-  this.state = { data: '' };
+    this.state = {
+      data: '',
+      error: 'No Data Found',
+    };
   }
 
   componentDidUpdate(prevProps) {
@@ -20,67 +23,78 @@ export default class MeterTextSum extends React.Component {
       this.setState({ data: '' });
       Meteor.call('getMeterbyDate', this.props.meterId,
           new Date(this.props.dateStart), new Date(this.props.dateEnd), (error, response) => {
-        if (error) {
-          // console.log(`SimpleLine${error}`);
-        } else {
-          // console.log(`res + for ID ${this.state.meterId}`);
-          if (!response.length) {
-            response = [];
-          }
+            if (error) {
+              // console.log(`SimpleLine${error}`);
+            } else {
+              // console.log(`res + for ID ${this.state.meterId}`);
+              if (!response.length) {
+                response = [];
+              }
 
-          // console.log(response);
+              // console.log(response);
 
-          self.setState({ data: response });
-        }
-      });
+              self.setState({ data: response });
+            }
+          });
     }
   }
-
 
   componentWillMount() {
     const self = this;
     Meteor.call('getMeterbyDate', this.props.meterId,
         new Date(this.props.dateStart), new Date(this.props.dateEnd), (error, response) => {
-      if (error) {
-        // console.log(`SimpleLine${error}`);
-      } else {
-        // console.log(`res + for ID ${this.state.meterId}`);
-        // console.log(response);
-        if (!response.length) {
-          response = [];
-        }
+          if (error) {
+            // console.log(`SimpleLine${error}`);
+          } else {
+            // console.log(`res + for ID ${this.state.meterId}`);
+            // console.log(response);
+            if (!response.length) {
+              response = [];
+            }
 
-        // console.log(response);
-        self.setState({ data: response });
-      }
-    });
+            // console.log(response);
+            self.setState({ data: response });
+          }
+        });
   }
 
   render() {
     return (this.state.data) ? this.renderGraph() : <Loader active>Getting data</Loader>;
   }
 
-
   renderGraph() {
     const data = this.state.data;
-    const maxPoint = _.max(data, entry => entry.max);
+    let sumPoint = _.pluck(data, 'mean');
+    let maxPoint = _.max(data, entry => entry.max);
+    let maxText = ''
+    let maxTime = 0
+
+    try{
+      sumPoint = sumPoint.reduce((total, val) => total + val);
+      sumPoint = `${sumPoint.toFixed(2) + this.props.unit }`
+      maxText = maxPoint.time.toLocaleString();
+      maxTime = `${maxPoint.max + this.props.unit }`
+    }
+    catch(err){
+      sumPoint = 'No Data Found';
+      maxTime = 'No Data Found';
+      maxText = 'No Data Found';
+    }
+
     const highlight = {
       backgroundColor: '#CCFF00', color: 'black',
       paddingLeft: '0.5rem', paddingRight: '0.5rem', borderRadius: '0.5rem'
     };
-    let sumPoint = _.pluck(data, 'mean');
-    sumPoint = sumPoint.reduce((total, val) => total + val);
-
     return (
         <Card fluid style={{ maxHeight: '4rem', color: '#fff', backgroundColor: '#383b4a', border: 'none' }}>
           <Card.Content style={{ textAlign: 'center', border: 'none' }}>
-            Max point: <span style={ highlight }>{`${maxPoint.max} ${this.props.unit}`}</span> on <span style={highlight}>{maxPoint.time.toLocaleString()}</span><br/>
-            Summation: <span style={highlight}>{`${sumPoint.toFixed(2)} ${this.props.unit}`}</span><br/>
+            Max point: <span style={highlight}>{ maxTime }</span> on <span
+              style={highlight}>{ maxText }</span><br/>
+            Summation: <span style={highlight}>{ sumPoint }</span><br/>
           </Card.Content>
         </Card>
-    );
+      )
   }
-
 }
 
 MeterTextSum.propTypes = {
