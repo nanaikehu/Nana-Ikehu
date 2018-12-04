ba845534ba0fd1c4fd27ef95e8b9c85f040539d5
Nguyen Banh <banhtuannguyen@gmail.com>
2018-12-04 08:51:58 -1000

    clean some code

--- app/imports/ui/components/HeatMap.jsx
+++ app/imports/ui/components/HeatMap.jsx
@@ -1,13 +1,10 @@
 // @flow
 
 import React, { Component } from 'react';
-import { Map, Marker, Popup,TileLayer, LayersControl, FeatureGroup } from 'react-leaflet';
+import { Map, TileLayer} from 'react-leaflet';
 import PropTypes from 'prop-types';
 import HeatmapLayer from 'react-leaflet-heatmap-layer';
-import { _ } from 'meteor/underscore';
-import defaultBuilding from '../../api/building_db/buildingCoor';
 import addressPoints from '../../api/building_db/HeatMapData';
-import { Meteor } from "meteor/meteor";
 
 
 
@@ -19,51 +16,13 @@ export default class Map2 extends Component {
     this.state.dateEnd = this.props.dateEnd;
   }
 
-  // componentDidUpdate(prevProps) {
-  //   // Typical usage (don't forget to compare props):
-  //
-  //   if (this.props.dateStart !== prevProps.dateStart || this.props.dateEnd !== prevProps.dateEnd) {
-  //     const self = this;
-  //     this.setState({ data: '' });
-  //     Meteor.call('sumByDate', new Date(this.props.dateStart), new Date(this.props.dateEnd), (error, response) => {
-  //       if (error) {
-  //         // console.log(`SimpleLine${error}`);
-  //       } else {
-  //         // console.log(`res + for ID ${this.state.meterId}`);
-  //         // console.log(response);
-  //
-  //         self.setState({ data: response });
-  //       }
-  //     });
-  //   }
-  // }
-  //
-  // componentWillMount() {
-  //   const self = this;
-  //   Meteor.call('sumByDate', new Date(this.props.dateStart), new Date(this.props.dateEnd), (error, response) => {
-  //     if (error) {
-  //       // console.log(`SimpleLine${error}`);
-  //     } else {
-  //        // console.log(response);
-  //        // console.log(this.props.dateStart);
-  //        // console.log(this.props.dateEnd);
-  //       self.setState({ data: response });
-  //     defaultBuilding.map( item =>{(this.state.data.buildings.map(e => {(e.name === item.name) ? (item.value = e.sum) : null}))});
-  //       self.setState({ HeatData: defaultBuilding });
-  //
-  //     }
-  //   });
-  // }
+
 
 
 
   render() {
     const position = [21.299677843574493, -157.81743038445714];
-    // const dataCoor = _.pluck(this.state.HeatData, "coor")
-    // const dataValue = _.pluck(this.state.HeatData, "value");
-    //
-    // console.log(dataCoor)
-    // console.log(dataValue)
+
     return (
 
         <Map center={position} zoom={17} minZoom={'15'} style={{ height: '600px' }}>
